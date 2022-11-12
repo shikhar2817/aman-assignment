ce89ebc4b3b19c42131c30cba08cb40d6e718b4d
shikhar2817 <shikharsingh0076@gmail.com>
2022-11-12 22:33:01 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [disable, setDisable] = useState(false);
+
+    const onSubmit = () => {
+        setDisable(true);
+        const data = { username: username, password: password };
+        console.log(data);
+        return data;
+    };
+
+    return (
+        <div>
+            <div style={{ marginLeft: "40%", marginTop: "20%" }}>
+                <div>
+                    <label>Username : </label>
+                    <input
+                        onChange={(e) => setUserName(e.target.value)}
+                        type="text"
+                        name="username"
+                        id="username-input"
+                    />
+                </div>
+                <br />
+                <div>
+                    <label>Password : </label>
+                    <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        name="password"
+                        id="password-input"
+                    />
+                </div>
+                <br />
+                <button disabled={disable} id="login-button" onClick={onSubmit}>
+                    Submit
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
